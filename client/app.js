801cf91340e40216ac706cf76b438e8f2aaaea50
vishal2796 <vishal.pb.27@gmail.com>
2023-01-12 19:32:55 +0530

    signin page

--- client/app.js
+++ client/app.js
@@ -1,12 +1,14 @@
-console.log("in signup page");
-const submitBtn = document.getElementById("create");
+const signUpBtn = document.getElementById("create");
+const signInBtn = document.getElementById("signin");
 const nameField = document.getElementById("name");
 const emailField = document.getElementById("email");
 const passwordField = document.getElementById("password");
 const errMsg = document.getElementById("errormsg");
 
-submitBtn.addEventListener("click", createAccount);
+if (signUpBtn) signUpBtn.addEventListener("click", createAccount);
+if (signInBtn) signInBtn.addEventListener("click", accountLogin);
 
+// signup
 async function createAccount(e) {
   e.preventDefault();
   try {
@@ -22,4 +24,22 @@ async function createAccount(e) {
       errMsg.style.display = "none";
     }, 5000);
   }
+}
+
+//signin
+async function accountLogin(e) {
+  console.log("values", emailField.value, passwordField.value);
+  e.preventDefault();
+  //   try {
+  //     let userData = {
+  //       email: emailField.value,
+  //       password: passwordField.value,
+  //     };
+  //     await axios.post("http://localhost:3000/signInUser", userData);
+  //   } catch (err) {
+  //     errMsg.style.display = "block";
+  //     setTimeout(() => {
+  //       errMsg.style.display = "none";
+  //     }, 5000);
+  //   }
 }
