ba580c9c59d5bd72520ef5963fedf5c8fa8b685e
vishal2796 <vishal.pb.27@gmail.com>
2023-01-24 19:03:55 +0530

    code optimization for deployment

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize").Sequelize;
 
-const sequelize = new Sequelize("expense-tracker", "root", "admin", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_SCHEMA,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: process.env.DATABASE_DIALECT,
+    host: process.env.DATABASE_HOST,
+  }
+);
 module.exports = sequelize;
