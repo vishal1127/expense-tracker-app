2526d2f5d91e45c461d07abce8249e67ec43162d
vishal2796 <vishal.pb.27@gmail.com>
2023-01-24 19:04:00 +0530

    code optimization for deployment

--- server/utils/database.js
+++ server/utils/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
   {
-    dialect: process.env.DATABASE_DIALECT,
+    dialect: "mysql",
     host: process.env.DATABASE_HOST,
   }
 );
